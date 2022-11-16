8dd41563919c3bcc2914ba5356912e3513e6185c
Adriana <adriana.oliversilva@gmail.com>
2022-11-16 18:36:26 -0300

    segundo comando

--- js/script.js
+++ js/script.js
@@ -21,13 +21,18 @@ function mostrarMenu() {
 function mostrarmenu() {
    let menu = document.getElementById("menu")
 
-   if(getComputedStyle(menu).display == "none"){
-       menu.style.display = "flex"
-   } else {
-       menu.style.display = "none"
+    if (getComputedStyle(menu).display == "none"){
+      menu.style.display = "flex"
+    } else{
+      menu.style.display = "none"
+      
+    }
+
+   
    }
 
-   }
+
+
 
    function cadastrarNewsletter() {
        alert("Usuário cadastrado com sucesso")
